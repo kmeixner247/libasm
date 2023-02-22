cff99c86f15637c9c6cfaebf26d730f17f2e718c
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-02-23 00:04:53 +0100

    ft_strcmp

---
+++ ft_strcmp.s
@@ -0,0 +1,32 @@
+;	strcmp(const char *s1, const char *s2);
+;	s1: rdi, s2: rsi
+	global _ft_strcmp
+
+	section .text
+_ft_strcmp:
+;protection?
+
+compare:
+	mov		dl, byte[rsi]
+	cmp		byte[rdi], dl
+	je		increment
+	jl		less
+	jg		more
+
+increment:
+	jz		same
+	inc		rdi
+	inc		rsi
+	jmp		compare
+
+less:
+	mov		rax, -1
+	ret
+
+more:
+	mov		rax, 1
+	ret
+
+same:
+	xor		rax, rax
+	ret
