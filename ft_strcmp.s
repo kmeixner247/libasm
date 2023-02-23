ed777aa0cd8bebbcfb0dd19a64e54fdf629fc137
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-02-23 19:55:25 +0100

    added comments:

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,36 +1,49 @@
+;------------------------------------------------------------------------------
+;	ft_strcmp
+;
+;	DESCRIPTION:
+;	Compares two strings. Returns 0 if equal, 1 if s1>s2 and -1 if s1<s2
+;
+;	C PROTOTYPE:
 ;	strcmp(const char *s1, const char *s2);
-;	s1: rdi, s2: rsi
-;	TODO:
-;	input protection
-;	comments
-;	'norm'
-	global _ft_strcmp
+;
+;	INPUT REGISTERS:
+;	rdi	:	s1
+;	rsi	:	s2
+;
+;	NOTES:
+;	Not protected against NULL input, because the original (man 3 strcmp) isn't
+;------------------------------------------------------------------------------
 
-	section .text
+	global	_ft_strcmp
+
+	section	.text
 _ft_strcmp:
-;protection?
 
-compare:
-	mov		dl, byte[rsi]
-	cmp		byte[rdi], dl
-	je		increment
-	jl		less
-	jg		more
+strcmp_cmp:
+	mov		dl, byte[rsi]	;	temp = *s2
+	cmp		byte[rdi], dl	;	compare *s1 with temp
+	je		strcmp_inc		;	if equal, increment and continue
+	jl		less			;	if *s1<temp, jump to less
+	jg		more			;	if *s1>temp, jump to more
 
-increment:
-	jz		same
-	inc		rdi
-	inc		rsi
-	jmp		compare
+strcmp_inc:
+	jz		same			;	if *s1 == 0, both strings are the same
+	inc		rdi				;	s1++
+	inc		rsi				;	s2++
+	jmp		strcmp_cmp		;	
 
 less:
-	mov		rax, -1
-	ret
+	mov		rax, -1			;	returning -1 if s1<s2
+	jmp		return
 
 more:
-	mov		rax, 1
-	ret
+	mov		rax, 1			;	returning 1 if s1>s2
+	jmp		return
 
 same:
-	xor		rax, rax
+	xor		rax, rax		;	returning 0 if s1 == s2
+	jmp		return
+
+return:
 	ret
